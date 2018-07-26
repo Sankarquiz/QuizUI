295cef200213dc4517d87e4ee3c03e47721d369b
Thanvez Ahmed <thanvez.ahmed@sonata-software.com>
2018-07-26 18:37:45 +0530

    Committed for Pulish quiz

---
+++ src/QuizUI/src/app/quiz-builder/set-pages/set-pages.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-set-pages',
+  templateUrl: './set-pages.component.html',
+  styleUrls: ['./set-pages.component.css']
+})
+export class SetPagesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
