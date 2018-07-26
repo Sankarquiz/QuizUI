e76fa10b3ab926c692c0ea8c1379b7cc700c9b43
SankarReddy <sankarreddy.m@sonata-software.com>
2018-07-26 16:49:34 +0530

    Adding service

--- src/QuizUI/src/environments/environment.ts
+++ src/QuizUI/src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  quizDefinitionUri: 'http://localhost:44370/define/',
 };
 
 /*
