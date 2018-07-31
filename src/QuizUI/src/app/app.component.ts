b2e3f1a111d185714b8244e45d11b1901ad68909
uttam.km <uttam.km@sonata-software.com>
2018-07-31 17:40:07 +0530

    Latest Quiz changes
    Latest Quiz changes

--- src/QuizUI/src/app/app.component.ts
+++ src/QuizUI/src/app/app.component.ts
@@ -8,13 +8,19 @@ import { FormDataService }  from './models/formData.service';
 })
 export class AppComponent {
   @Input() formData;
+  //@Input() registrationData;
+  //@Input() sponserData;
+  //@Input() questionData;
     
     constructor(private formDataService: FormDataService) {
     }
 
     ngOnInit() {
     //alert('jsjsj');
-        this.formData = this.formDataService.getFormData();
+        this.formData = this.formDataService.getFormData(); 
+       // this.registrationData  = this.formDataService.getRegistrationData();
+        //this.sponserData = this.formDataService.getSponsorData();
+        //this.questionData = this.formDataService.getQuestionData();
     //alert('jsjsj-2');      
     }
 } 
--- src/QuizUI/src/app/models/formData.service.ts
+++ src/QuizUI/src/app/models/formData.service.ts
@@ -1,24 +1,25 @@
 import { Injectable } from '@angular/core';
 
-import { QuizDefinition, QuizSet, RegistrationFields, SponserDetail } from './QuizDefinition';
+import { FormData, QuizDefinition, QuizSet, RegistrationFields, SponserDetail } from './QuizDefinition';
 
 @Injectable()
 export class FormDataService {
 
-  private formData: QuizDefinition = new QuizDefinition();
-  private registrationData: RegistrationFields = new RegistrationFields();
-  private sponserData: SponserDetail = new SponserDetail();
-  private questionData: QuizSet = new QuizSet();
+  private formData: FormData  = new FormData ();
+  //private registrationData: RegistrationFields = new RegistrationFields();
+  //private sponserData: SponserDetail = new SponserDetail();
+ // private questionData: QuizSet = new QuizSet();
   constructor() {
   }
 
   getQuizDefinition(): QuizDefinition {
     // Return the Quiz data
+    debugger;
     var quizDefinition: QuizDefinition = {
       QuizName: this.formData.QuizName,
       QuizDomainHost: this.formData.QuizDomainHost,
       QuizType: this.formData.QuizType,
-      NoOfQuestions: this.formData.NoOfQuestions,
+      NoOfQuestion: this.formData.NoOfQuestion,
       NoOfParticipants: this.formData.NoOfParticipants,
       QuizDurationHour: this.formData.QuizDurationHour,
       QuizDurationTime: this.formData.QuizDurationTime,
@@ -37,7 +38,7 @@ export class FormDataService {
       Status: this.formData.Status,
       Stage: this.formData.Stage,
       RegistrationFields: this.formData.RegistrationFields,
-      SponsorList: this.formData.SponsorList,
+      SponsorList: this.formData.SponsorList
     };
     return quizDefinition;
   }
@@ -46,7 +47,7 @@ export class FormDataService {
     this.formData.QuizName = data.QuizName;
     this.formData.QuizDomainHost = data.QuizDomainHost;
     this.formData.QuizType = data.QuizType;
-    this.formData.NoOfQuestions = data.NoOfQuestions;
+    this.formData.NoOfQuestion = data.NoOfQuestion;
     this.formData.NoOfParticipants = data.NoOfParticipants;
     this.formData.QuizDurationHour = data.QuizDurationHour;
     this.formData.QuizDurationTime = data.QuizDurationTime;
@@ -62,87 +63,92 @@ export class FormDataService {
     this.formData.IsQuizAutoEvaluate = data.IsQuizAutoEvaluate;
     this.formData.ShowScoreAfterAttempt = data.ShowScoreAfterAttempt;
     this.formData.PostScoreOnSocialMedia = data.PostScoreOnSocialMedia;
+    this.formData.RegistrationFields= data.RegistrationFields;
+    this.formData.SponsorList= data.SponsorList;
   }
 
 
   getQuizSet(): QuizSet {
     var quizSet: QuizSet = {
-      QuizName: this.questionData.QuizName,
-      QuizType: this.questionData.QuizType,
-      QuestionNo: this.questionData.QuestionNo,
-      QuestionText: this.questionData.QuestionText,
-      ImageUrl: this.questionData.ImageUrl,
-      AnswerType: this.questionData.AnswerType,
-      Answer: this.questionData.Answer,
-      Score: this.questionData.Score,
-      Option1: this.questionData.Option1,
-      Option2: this.questionData.Option2,
-      Option3: this.questionData.Option3,
-      Option4: this.questionData.Option4
+      QuizName: this.formData.QuizName,
+      QuizType: this.formData.QuizType,
+      QuestionNo: this.formData.QuestionNo,
+      QuestionText: this.formData.QuestionText,
+      ImageUrl: this.formData.ImageUrl,
+      AnswerType: this.formData.AnswerType,
+      Answer: this.formData.Answer,
+      Score: this.formData.Score,
+      Option1: this.formData.Option1,
+      Option2: this.formData.Option2,
+      Option3: this.formData.Option3,
+      Option4: this.formData.Option4
     };
     return quizSet;
   }
 
   setQuizSet(data: QuizSet) {
-    this.questionData.QuizName = data.QuizName;
-    this.questionData.QuizType = data.QuizType;
-    this.questionData.QuestionNo = data.QuestionNo;
-    this.questionData.QuestionText = data.QuestionText;
-    this.questionData.ImageUrl = data.ImageUrl;
-    this.questionData.AnswerType = data.AnswerType;
-    this.questionData.Answer = data.Answer;
-    this.questionData.Score = data.Score;
-    this.questionData.Option1 = data.Option1;
-    this.questionData.Option2 = data.Option2;
-    this.questionData.Option3 = data.Option3;
-    this.questionData.Option4 = data.Option4;
+    this.formData.QuizName = data.QuizName;
+    this.formData.QuizType = data.QuizType;
+    this.formData.QuestionNo = data.QuestionNo;
+    this.formData.QuestionText = data.QuestionText;
+    this.formData.ImageUrl = data.ImageUrl;
+    this.formData.AnswerType = data.AnswerType;
+    this.formData.Answer = data.Answer;
+    this.formData.Score = data.Score;
+    this.formData.Option1 = data.Option1;
+    this.formData.Option2 = data.Option2;
+    this.formData.Option3 = data.Option3;
+    this.formData.Option4 = data.Option4;
   }
 
   getRegistrationFields(): RegistrationFields {
     var quizRegistrationFields: RegistrationFields = {
-      TeamName: this.registrationData.TeamName,
-      EmailId: this.registrationData.EmailId,
-      ValidateEmail: this.registrationData.ValidateEmail,
-      ContestantName: this.registrationData.ContestantName,
-      PhoneNumber: this.registrationData.PhoneNumber,
-      ContactAddress: this.registrationData.ContactAddress
+      IsTeamName: this.formData.IsTeamName,
+      IsEmail: this.formData.IsEmail,
+      IsValidateEmail: this.formData.IsValidateEmail,
+      IsContestantName: this.formData.IsContestantName,
+      IsPhone: this.formData.IsPhone,
+      IsContact: this.formData.IsContact,
+      RulesAndRegulations: this.formData.RulesAndRegulations
     };
     return quizRegistrationFields;
   }
 
   setRegistrationFields(data: RegistrationFields) {
-    this.registrationData.TeamName = data.TeamName;
-    this.registrationData.EmailId = data.EmailId;
-    this.registrationData.ValidateEmail = data.ValidateEmail;
-    this.registrationData.ContestantName = data.ContestantName;
-    this.registrationData.PhoneNumber = data.PhoneNumber;
-    this.registrationData.ContactAddress = data.ContactAddress
+    this.formData.IsTeamName = data.IsTeamName;
+    this.formData.IsEmail = data.IsEmail;
+    this.formData.IsValidateEmail = data.IsValidateEmail;
+    this.formData.IsContestantName = data.IsContestantName;
+    this.formData.IsPhone = data.IsPhone;
+    this.formData.IsContact = data.IsContact;
+    this.formData.RulesAndRegulations = data.RulesAndRegulations;
   }
 
   getSponserFields(): SponserDetail {
     var sponserDetail: SponserDetail = {
-      Path: this.sponserData.Path,
-      Position: this.sponserData.Position
+      Path: this.formData.Path,
+      Position: this.formData.Position
     };
     return sponserDetail;
   }
 
   setSponserFields(data: SponserDetail) {
-    this.sponserData.Path = data.Path;
-    this.sponserData.Position = data.Position;
+    this.formData.Path = data.Path;
+    this.formData.Position = data.Position;
   }
 
   getFormData(): QuizDefinition {
+  debugger;
     return this.formData;
   }
-  getRegistrationData(): RegistrationFields {
-    return this.registrationData;
-  }
-  getSponsorData(): SponserDetail {
-    return this.sponserData;
-  }
-  getQuestionData(): QuizSet {
-    return this.questionData;
-  }
+ // getRegistrationData(): RegistrationFields {
+  //  return this.registrationData;
+  //}
+  //getSponsorData(): SponserDetail {
+  //  return this.sponserData;
+  //}
+  //getQuestionData(): QuizSet {
+ //   return this.questionData;
+ // }
 }
 
