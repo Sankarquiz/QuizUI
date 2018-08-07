cf9b907dbe11aa8cc7e110a85051c9a1b9fce5e2
SankarReddy <sankarreddy.m@sonata-software.com>
2018-08-08 00:15:08 +0530

    fix

--- src/QuizWebApi/Models/Admin/QuizSet.cs
+++ src/QuizWebApi/Models/Admin/QuizSet.cs
@@ -8,7 +8,7 @@
         public string ImageUrl { get; set; }
         public string AnswerType { get; set; }
         public string Answer { get; set; }
-        public string Score { get; set; }
+        public int Score { get; set; }
         public string Option1 { get; set; }
         public string Option2 { get; set; }
         public string Option3 { get; set; }
