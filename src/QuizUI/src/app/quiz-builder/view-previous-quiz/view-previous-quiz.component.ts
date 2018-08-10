7adf794c25550a63b23db0f68213bfc3c0f89fcd
sarchandra <sarchandra@users.noreply.github.com>
2018-08-10 13:09:20 +0530

    routing view to page
    routing view to page

--- src/QuizUI/src/app/quiz-builder/view-previous-quiz/view-previous-quiz.component.ts
+++ src/QuizUI/src/app/quiz-builder/view-previous-quiz/view-previous-quiz.component.ts
@@ -1,7 +1,9 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { forEach } from '@angular/router/src/utils/collection';
 import { QuizDefinition } from '../../models/QuizDefinition';
 import { QuizDetailsService } from '../../services/service-getquizdetails';
+import { FormDataService } from '../../models/formData.service';
 
 @Component({
   selector: 'app-view-previous-quiz',
@@ -11,7 +13,11 @@ import { QuizDetailsService } from '../../services/service-getquizdetails';
 export class ViewPreviousQuizComponent implements OnInit {
 
   quizDetails = new Array<QuizDefinition>();
-  constructor(private _getAllQuizDetails: QuizDetailsService) { }
+
+  constructor(private router: Router,
+    private _getAllQuizDetails: QuizDetailsService,
+    private formDataService: FormDataService) { }
+    
 
   ngOnInit() {
     this._getAllQuizDetails.GetAllQuizData()
@@ -21,5 +27,22 @@ export class ViewPreviousQuizComponent implements OnInit {
   }
   PopulateResults(result: any): any {
     this.quizDetails = result;
+  }
+
+  quizScreen(result: any) {
+    debugger;
+    this.formDataService.setQuizDefinition(result);
+    let reurl = "/quiz-builder/create-quiz/define-the-Quiz";
+    if (result.stage.toLowerCase() == 'define')
+      reurl = "/quiz-builder/create-quiz/define-the-Quiz";
+    if (result.stage.toLowerCase() == 'registration')
+      reurl = "/quiz-builder/create-quiz/Registration";
+    if (result.stage.toLowerCase() == 'setlogo')
+      reurl = "/quiz-builder/create-quiz/set-logos-group";
+    if (result.stage.toLowerCase() == 'setquestion')
+      reurl = "/quiz-builder/create-quiz/set-the-quiz";
+    if (result.stage.toLowerCase() == 'setpage')
+      reurl = "/quiz-builder/create-quiz/set-pages";
+    this.router.navigateByUrl(reurl);
   }
 }
