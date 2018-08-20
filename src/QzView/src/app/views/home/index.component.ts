4eacd932839303c85431b07b5dc473a870ea30d9
sarchandra <sarchandra@users.noreply.github.com>
2018-08-20 22:21:05 +0530

    modifed
    mofied

--- src/QzView/src/app/views/home/index.component.ts
+++ src/QzView/src/app/views/home/index.component.ts
@@ -1,37 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnChanges } from '@angular/core';
+import { QuizDefinition, RegistrationFields } from '../../models/QuizDefinition';
 import { FormDataService } from '../../models/formData.service';
 import { Router } from '@angular/router';
-import { QuizAdv, UserDataModel } from '../../models/Registration';
 import { QuizDetailsService } from '../../services/service-getquizdetails';
-import { QuizDefinition } from '../../models/QuizDefinition'; 
+import { Observable } from 'rxjs';
 
+import { DatePipe } from '@angular/common';
 @Component({
+  selector: 'app-home-index',
   templateUrl: 'index.component.html'
 })
-export class HomeIndexComponent implements OnInit {
-  constructor(private router: Router, private formDataService: FormDataService, private _getquizdetails: QuizDetailsService) { }
+export class HomeIndexComponent {
 
-  quizAdv: any;
-  quizdefinition = new Array<QuizDefinition>();
-  userdetails: UserDataModel;
-  ngOnInit() {    
-    this._getquizdetails.GetActiveQuizData()
-      .subscribe((result: any) => {
-        this.quizdefinition = result;
-      });
-    this.userdetails = this.formDataService.getUserData();
-  }
-  SelectQuiz(index) {
-    debugger;
-    this.quizAdv = new QuizAdv();
-    this.quizAdv.quizName = this.quizdefinition[index].quizName;
-    this.quizAdv.quizType = this.quizdefinition[index].quizType;
-    this.formDataService.setquizadv(this.quizAdv);
-    if (this.userdetails.teamName && this.userdetails.role) {
-      this.router.navigate(['/quiz/quizrunner/viewquiz']);
-    }
-    else {
-      this.router.navigate(['/user-registration']);
-    }
-  }
+  
 }
