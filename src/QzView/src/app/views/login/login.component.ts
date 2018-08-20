9fe021b1fa7a5448e51e2bbbb6d6c1038e6c6e94
sarchandra <sarchandra@users.noreply.github.com>
2018-08-20 19:50:33 +0530

    login page
    login page

--- src/QzView/src/app/views/login/login.component.ts
+++ src/QzView/src/app/views/login/login.component.ts
@@ -1,7 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { QuizDetailsService } from '../../services/service-getquizdetails';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { FormDataService } from '../../models/formData.service';
+import { UserRegistration } from '../../models/Registration';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: 'login.component.html'
 })
-export class LoginComponent { }
+export class LoginComponent {
+  username: string;
+  password: string;
+  result: Observable<any>;
+  userDetails = new UserRegistration();
+  constructor(private _register: QuizDetailsService, private router: Router, private formDataService: FormDataService) { }
+
+  ngOnInit() {
+    this.username = '';
+    this.password = '';
+  }
+  Login() {
+    debugger;
+    if (this.username && this.password) {
+      this._register.Login(this.username, this.password)
+        .subscribe((response: any) => {
+          this.result = response;
+          if (response) {
+            this.userDetails.teamName = this.username;
+            this.userDetails.role = response[0].role;
+            this.formDataService.setUserData(this.userDetails);
+            this.router.navigate(['/quizadds']);
+          } else {
+            alert('Not Saved.');
+          }
+        });
+    }
+  }
+}
