10eb40779480250d887742df174e8d784546b2ca
SankarReddy <sankarreddy.m@sonata-software.com>
2018-08-21 00:26:48 +0530

    home page binding

--- src/QzView/src/app/views/home/index.component.ts
+++ src/QzView/src/app/views/home/index.component.ts
@@ -1,9 +1,9 @@
-import { Component, OnInit, OnChanges } from '@angular/core';
-import { QuizDefinition, RegistrationFields } from '../../models/QuizDefinition';
+import { Component, OnInit } from '@angular/core';
 import { FormDataService } from '../../models/formData.service';
 import { Router } from '@angular/router';
+import { QuizAdv, UserDataModel } from '../../models/Registration';
 import { QuizDetailsService } from '../../services/service-getquizdetails';
-import { Observable } from 'rxjs';
+import { QuizDefinition } from '../../models/QuizDefinition';
 
 import { DatePipe } from '@angular/common';
 @Component({
@@ -11,6 +11,29 @@ import { DatePipe } from '@angular/common';
   templateUrl: 'index.component.html'
 })
 export class HomeIndexComponent {
+  constructor(private router: Router, private formDataService: FormDataService, private _getquizdetails: QuizDetailsService) { }
 
-  
+  quizAdv: any;
+  quizdefinition = new Array<QuizDefinition>();
+  userdetails: UserDataModel;
+  ngOnInit() {
+    this._getquizdetails.GetActiveQuizData()
+      .subscribe((result: any) => {
+        this.quizdefinition = result;
+      });
+    this.userdetails = this.formDataService.getUserData();
+  }
+  SelectQuiz(index) {
+    debugger;
+    this.quizAdv = new QuizAdv();
+    this.quizAdv.quizName = this.quizdefinition[index].quizName;
+    this.quizAdv.quizType = this.quizdefinition[index].quizType;
+    this.formDataService.setquizadv(this.quizAdv);
+    if (this.userdetails.teamName && this.userdetails.role) {
+      this.router.navigate(['/quiz-runner']);
+    }
+    else {
+      this.router.navigate(['/user-registration']);
+    }
+  }
 }
--- src/QzView/src/app/views/home/quizhome.module.ts
+++ src/QzView/src/app/views/home/quizhome.module.ts
@@ -1,3 +1,4 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { ChartsModule } from 'ng2-charts/ng2-charts';
@@ -5,15 +6,26 @@ import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { ButtonsModule } from 'ngx-bootstrap/buttons';
 import { HomeIndexComponent } from './index.component';
 import { QuizHomeRoutingModule } from './quizhome.routing.module';
- 
+import { FormDataService } from '../../models/formData.service';
+import { QuizDetailsService } from '../../services/service-getquizdetails';
+import { HttpClientModule } from '@angular/common/http';
+
 @NgModule({
   imports: [
     FormsModule,
+    HttpClientModule,
+    CommonModule,
     QuizHomeRoutingModule,
     ChartsModule,
     BsDropdownModule,
     ButtonsModule.forRoot()
   ],
-  declarations: [HomeIndexComponent ]
+  declarations: [
+    HomeIndexComponent
+  ],
+  providers: [
+    QuizDetailsService,
+    FormDataService
+  ]
 })
 export class QuizHomeModule { }
