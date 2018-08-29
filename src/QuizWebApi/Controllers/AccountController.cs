8fd348146871532b9f0064394de19a9b15422bae
SankarReddy <sankarreddy.m@sonata-software.com>
2018-08-29 11:57:03 +0530

    Adding validation

--- src/QuizWebApi/Controllers/AccountController.cs
+++ src/QuizWebApi/Controllers/AccountController.cs
@@ -1,15 +1,10 @@
 ﻿using Couchbase.N1QL;
 using Microsoft.AspNetCore.Mvc;
 using QuizWebApi.Models.User;
+using QuizWebApi.Utilities;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using System.Net.Mail;
-using System;
-using System.Text;
-using QuizWebApi.Models.Common;
-using Microsoft.Extensions.Options;
-using QuizWebApi.Utilities;
 
 namespace QuizWebApi.Controllers
 {
@@ -17,16 +12,16 @@ namespace QuizWebApi.Controllers
     [ApiController]
     public class AccountController : ControllerBase
     {
-        public EmailManager Email { get; }
+        public EmailManager _email { get; }
         public AccountController(EmailManager email)
         {
-            Email = email;
+            _email = email;
         }
 
         [HttpPost]
         public async Task<IActionResult> Register([FromBody]UserRegistration user)
         {
-            var query = string.Format(@"SELECT {0}.* FROM {0} WHERE documentType=""{1}"" and quizName=""{3}"" and email=""{2}"" or email2=""{2}"" or email3=""{2}"" ",
+            var query = string.Format(@"SELECT {0}.* FROM {0} WHERE documentType=""{1}"" and quizName=""{3}"" and (email=""{2}"" or email2=""{2}"" or email3=""{2}"") ",
                             CouchbaseHelper.Bucket, "Register", user.Email, user.QuizName);
             var req = new QueryRequest(query);
             var res = await CouchbaseHelper.CouchbaseClient.GetByQueryAsync<UserRegistration>(req);
@@ -46,6 +41,15 @@ namespace QuizWebApi.Controllers
 
             user.DocumentType = "Register";
             var result = await CouchbaseHelper.CouchbaseClient.UpsertAsync(user.Email + user.QuizName, user);
+            _email.RegisterQuiz(user.ContestantName, user.ContestantName, user.TeamName, user.QuizName, user.Email);
+            if (!string.IsNullOrWhiteSpace(user.Email2))
+            {
+                _email.RegisterQuiz(user.ContestantName, user.ContestantName2, user.TeamName, user.QuizName, user.Email2);
+            }
+            if (!string.IsNullOrWhiteSpace(user.Email3))
+            {
+                _email.RegisterQuiz(user.ContestantName, user.ContestantName3, user.TeamName, user.QuizName, user.Email3);
+            }
             return Ok(result);
         }
 
@@ -59,20 +63,21 @@ namespace QuizWebApi.Controllers
             }
 
             var result = await CouchbaseHelper.CouchbaseClient.UpsertAsync(signup.Email, signup);
-            SendMail(signup);
+            _email.SignUpEmail("", signup.Email);
             return Ok(result);
         }
 
         [HttpGet("{email}")]
         public async Task<IActionResult> ActivateSignUp(string email)
         {
-            var response = await CouchbaseHelper.CouchbaseClient.GetByKeyAsync<SignUp>(email);
+            string dcrEmail = CryptoEngine.Decrypt(email);
+            var response = await CouchbaseHelper.CouchbaseClient.GetByKeyAsync<SignUp>(dcrEmail);
             if (!response.Success)
             {
                 return NotFound();
             }
             response.Value.Status = "active";
-            var result = await CouchbaseHelper.CouchbaseClient.UpsertAsync(email, response);
+            var result = await CouchbaseHelper.CouchbaseClient.UpsertAsync(dcrEmail, response.Value);
             return Ok(result);
         }
 
@@ -102,7 +107,7 @@ namespace QuizWebApi.Controllers
         [HttpGet]
         public async Task<IActionResult> GetRegisteredQuizDetails(string email)
         {
-            var query = string.Format(@"SELECT {0}.* FROM {0} where documentType=""{1}"" and email=""{2}"" or email2=""{2}"" or email3=""{2}"" ",
+            var query = string.Format(@"SELECT {0}.* FROM {0} where documentType=""{1}"" and (email=""{2}"" or email2=""{2}"" or email3=""{2}"") ",
                 CouchbaseHelper.Bucket, "Register", email);
             var req = new QueryRequest(query);
             var result = await CouchbaseHelper.CouchbaseClient.GetByQueryAsync<UserRegistration>(req);
@@ -110,24 +115,24 @@ namespace QuizWebApi.Controllers
         }
 
         #region Private Methods
-        private void SendMail(SignUp signup)
-        {
-            var host = Request.Scheme + "://" + Request.Host + "/api/quiz/ActivateSignUp?email=" + signup.Email;
-            
-            MailMessage mail = new MailMessage();
-            SmtpClient client = new SmtpClient("smtp.gmail.com");
-            mail.From = new MailAddress("quiz@knowledgevysya.com");
-            mail.To.Add(signup.Email);
-            mail.Subject = "Activation email from KnowledgeVysya.";
-            mail.Body = "Please click on the below link to activate your account, "+ host;
-            client.Port = 25;
-            client.Credentials = new System.Net.NetworkCredential("me", "password");
-            client.UseDefaultCredentials = false; 
-            client.EnableSsl = true;
-            client.Host = "smtp.gmail.com";
- 
-            client.Send(mail);
-        }
+        //private void SendMail(SignUp signup)
+        //{
+        //    var host = Request.Scheme + "://" + Request.Host + "/api/quiz/ActivateSignUp?email=" + signup.Email;
+
+        //    MailMessage mail = new MailMessage();
+        //    SmtpClient client = new SmtpClient("smtp.gmail.com");
+        //    mail.From = new MailAddress("quiz@knowledgevysya.com");
+        //    mail.To.Add(signup.Email);
+        //    mail.Subject = "Activation email from KnowledgeVysya.";
+        //    mail.Body = "Please click on the below link to activate your account, "+ host;
+        //    client.Port = 25;
+        //    client.Credentials = new System.Net.NetworkCredential("me", "password");
+        //    client.UseDefaultCredentials = false; 
+        //    client.EnableSsl = true;
+        //    client.Host = "smtp.gmail.com";
+
+        //    client.Send(mail);
+        //}
         #endregion
     }
 }
