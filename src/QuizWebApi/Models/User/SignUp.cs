adf88dd89e5ee1bf82faaa153feb3613ff9ce564
sarchandra <sarchandra@users.noreply.github.com>
2018-08-31 15:30:00 +0530

    modified
    modified

--- src/QuizWebApi/Models/User/SignUp.cs
+++ src/QuizWebApi/Models/User/SignUp.cs
@@ -1,10 +1,17 @@
 ﻿namespace QuizWebApi.Models.User
 {
-    public class SignUp: Status
+    public class SignUp : Status
     {
         public string Email { get; set; }
         public string Password { get; set; }
         public string Status { get; set; }
         public string Role { get; set; }
+        public string Source { get; set; }
+        public string DocumentType { get; set; }
+
+        public SignUp()
+        {
+            DocumentType = "user";
+        }
     }
 }
